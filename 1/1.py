b95a68f1fbccb07d64b1282894eb2489af04092b
unknown <szymonjarzab1997@gmail.com>
2020-12-07 18:21:01 +0100

    day 1 refactor

--- 1/1.py
+++ 1/1.py
@@ -1,8 +1,15 @@
+tab = []
+
 with open('input.txt') as file:
-    tab = []
     for line in file:
         tab.append(int(line.strip()))
+
+
+def find_pair(tab, value):
     for i in range(0, len(tab)):
         for j in range(i, len(tab)):
             if tab[i] + tab[j] == 2020:
-                print(tab[i] * tab[j])
+                return tab[i] * tab[j]
+
+
+print(find_pair(tab, 2020))
--- 1/2.py
+++ 1/2.py
@@ -1,9 +1,16 @@
+tab = []
+
 with open('input.txt') as file:
-    tab = []
     for line in file:
         tab.append(int(line.strip()))
+
+
+def find_three(tab, value):
     for i in range(0, len(tab)):
         for j in range(i, len(tab)):
             for k in range(j, len(tab)):
                 if tab[i] + tab[j] + tab[k] == 2020:
-                    print(tab[i] * tab[j] * tab[k])
+                    return tab[i] * tab[j] * tab[k]
+
+
+print(find_three(tab, 2020))
