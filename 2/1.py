8693072a602cf11220b94e2ed832afb0b8c614c5
unknown <szymonjarzab1997@gmail.com>
2020-12-07 18:55:25 +0100

    day 2 refactor

--- 2/1.py
+++ 2/1.py
@@ -1,15 +1,21 @@
+def validate_password(password):
+    c = 0
+    password = password.split(' ')
+    password[0] = password[0].split('-')
+    password[1] = password[1][0]
+    for letter in password[2]:
+        if letter == password[1]:
+            c += 1
+    if int(password[0][0]) <= c <= int(password[0][1]):
+        return True
+    return False
+
+
 with open('input.txt') as file:
     counter = 0
     for line in file:
-        c = 0
-        l = line.strip().split(' ')
-        l[0] = l[0].split('-')
-        l[1] = l[1][0]
-        for letter in l[2]:
-            if letter == l[1]:
-                c += 1
-
-        if c >= int(l[0][0]) and c <= int(l[0][1]):
+        if validate_password(line.strip()):
             counter += 1
+
 
 print(counter)
--- 2/2.py
+++ 2/2.py
@@ -1,16 +1,23 @@
+def validate_password(password):
+    c = 0
+    data = password.split(' ')
+    data[0] = data[0].split('-')
+    data[1] = data[1][0]
+    try:
+        if data[2][int(data[0][0]) - 1] == data[1] and data[2][int(data[0][1]) - 1] == data[1]:
+            return False
+        elif data[2][int(data[0][0]) - 1] == data[1] or data[2][int(data[0][1]) - 1] == data[1]:
+            return True
+    except IndexError:
+        return False
+    return False
+
+
 with open('input.txt') as file:
     counter = 0
     for line in file:
-        c = 0
-        data = line.strip().split(' ')
-        data[0] = data[0].split('-')
-        data[1] = data[1][0]
-        try:
-            if data[2][int(data[0][0]) - 1] == data[1] and data[2][int(data[0][1]) - 1] == data[1]:
-                pass
-            elif data[2][int(data[0][0]) - 1] == data[1] or data[2][int(data[0][1]) - 1] == data[1]:
-                counter += 1
-        except IndexError:
-            pass
+        if validate_password(line.strip()):
+            counter += 1
+
 
 print(counter)
