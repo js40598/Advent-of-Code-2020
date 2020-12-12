ab18389d76d3712bf808524cc007ac738a271a93
unknown <szymonjarzab1997@gmail.com>
2020-12-12 12:42:54 +0100

    day 12 refactor

--- 12/1.py
+++ 12/1.py
@@ -6,7 +6,6 @@ with open('input.txt') as file:
 
 for i in range(0, len(tab)):
     tab[i] = [tab[i][0], tab[i][1::]]
-    print(tab[i])
 
 
 def rotate(forward, side, degree):
@@ -21,16 +20,13 @@ def rotate(forward, side, degree):
     return forwards[int(degree / 90)]
 
 
-forward = 'E'
-current_position = [0, 0]
-directions = {
-    'N': [1, 0],
-    'E': [0, 1],
-    'S': [-1, 0],
-    'W': [0, -1],
-}
-
-for command in tab:
+def execute_command(command, current_position, forward):
+    directions = {
+        'N': [1, 0],
+        'E': [0, 1],
+        'S': [-1, 0],
+        'W': [0, -1],
+    }
     if command[0] in 'NESW':
         current_position = [current_position[0] + directions[command[0]][0] * int(command[1]),
                             current_position[1] + directions[command[0]][1] * int(command[1])]
@@ -39,7 +35,11 @@ for command in tab:
     elif command[0] in 'F':
         current_position = [current_position[0] + directions[forward][0] * int(command[1]),
                             current_position[1] + directions[forward][1] * int(command[1])]
-    else:
-        print('sth')
-print(current_position)
+    return forward, current_position
+
+
+forward = 'E'
+current_position = [0, 0]
+for command in tab:
+    forward, current_position = execute_command(command, current_position, forward)
 print(abs(current_position[0]) + abs(current_position[1]))
--- 12/2.py
+++ 12/2.py
@@ -26,18 +26,13 @@ def rotate_waypoint(ship_position, waypoint_position, side, degree):
     return [ship_position[0] + relative_waypoint_position[0], ship_position[1] + relative_waypoint_position[1]]
 
 
-
-forward = 'E'
-ship_position = [0, 0]
-waypoint_position = [1, 10]
-directions = {
-    'N': [1, 0],
-    'E': [0, 1],
-    'S': [-1, 0],
-    'W': [0, -1],
-}
-
-for command in tab:
+def execute_command(command, ship_position, waypoint_position):
+    directions = {
+        'N': [1, 0],
+        'E': [0, 1],
+        'S': [-1, 0],
+        'W': [0, -1],
+    }
     if command[0] in 'NESW':
         waypoint_position = [waypoint_position[0] + directions[command[0]][0] * int(command[1]),
                              waypoint_position[1] + directions[command[0]][1] * int(command[1])]
@@ -45,17 +40,15 @@ for command in tab:
         waypoint_position = rotate_waypoint(ship_position, waypoint_position, command[0], int(command[1]))
     elif command[0] in 'F':
         vector = [-ship_position[0] + waypoint_position[0], -ship_position[1] + waypoint_position[1]]
-        print('vector: ', vector)
         ship_position = [ship_position[0] + vector[0] * int(command[1]),
                          ship_position[1] + vector[1] * int(command[1])]
         waypoint_position = [waypoint_position[0] + vector[0] * int(command[1]),
                              waypoint_position[1] + vector[1] * int(command[1])]
-    else:
-        print('sth')
-    print(' ')
-    print('command: ', command)
-    print('ship_position: ', ship_position)
-    print('waypoint_position: ', waypoint_position)
-print(ship_position)
-print(waypoint_position)
+    return ship_position, waypoint_position
+
+
+ship_position = [0, 0]
+waypoint_position = [1, 10]
+for command in tab:
+    ship_position, waypoint_position = execute_command(command, ship_position, waypoint_position)
 print(abs(ship_position[0]) + abs(ship_position[1]))
